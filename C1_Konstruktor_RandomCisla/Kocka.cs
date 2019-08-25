84b70262ce5c462ab9812b7827f4f52a313a2fc0
Filip <Filip@FILIP-DPC>
2019-08-25 16:11:29 +0200

    project

--- C1_Konstruktor_RandomCisla/Kocka.cs
+++ C1_Konstruktor_RandomCisla/Kocka.cs
@@ -9,17 +9,38 @@ namespace C1_Konstruktor_RandomCisla
     class Kocka
     {
         private Random random;
-        private int pocetStien;
+        private sbyte pocetStien; //nepredpokladame ze sa bude hadzat viac ako 127 stenovou kockou....
 
-        public Kocka(int aPocetStien)
+
+        public Kocka(sbyte aPocetStien)
         {
             this.pocetStien = aPocetStien;
             random = new Random();
+            validation();
         }
 
-        public int getPocetStien()
+        private void validation()
+        {
+            if (pocetStien < 0)
+            {
+                pocetStien *= -1;
+            }
+        }
+
+        public sbyte getPocetStien()
         {
             return pocetStien;
+        }
+
+        public sbyte getRandomHod()
+        {
+            return Convert.ToSByte(random.Next(1, pocetStien + 1));
+            
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Ďakujem za vstup\n zadali ste kocku s {0} stenami", getPocetStien());
         }
     }
 }
--- C1_Konstruktor_RandomCisla/Program.cs
+++ C1_Konstruktor_RandomCisla/Program.cs
@@ -8,10 +8,32 @@ namespace C1_Konstruktor_RandomCisla
 {
     class Program
     {
+        public const sbyte TEEN = 10;
+
+
         static void Main(string[] args)
         {
-            Kocka kocka = new Kocka(6);
-          
+            sbyte stenyKocky = 0;
+            Console.WriteLine("Zadaj kolko stennou kockou chceš hadzat");
+            
+
+            if (sbyte.TryParse(Console.ReadLine(), out stenyKocky))
+            {
+                Kocka kocka = new Kocka(stenyKocky);
+
+                Console.WriteLine(kocka.ToString());
+                for (int i = 0; i < TEEN; i++)
+                {
+                    Console.Write(kocka.getRandomHod() + " ");
+                }
+
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("error pri vstupe\nPravdepodobne ste nezadali cislo alebo ste vymysleli brutalnu kocku");
+                Console.ReadKey();
+            } 
         }
     }
 }
